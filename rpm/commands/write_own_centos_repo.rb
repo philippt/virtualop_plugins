d5b90100ecf228e3ecd63f8a8b589f9aad9e72bb
Philipp T <philipp@virtualop.org>
2012-01-09 12:29:21 +0100

    rpm/write_own_centos_repo

---
+++ rpm/commands/write_own_centos_repo.rb
@@ -0,0 +1,9 @@
+param :machine
+
+on_machine do |vm, params|
+  target_file_name = '/etc/yum.repos.d/CentOS-Base.repo'
+  
+  vm.rm("file_name" => target_file_name) if vm.file_exists("file_name" => target_file_name)
+    
+  process_template(:centos_base_repo, vm, target_file_name, binding())
+end
