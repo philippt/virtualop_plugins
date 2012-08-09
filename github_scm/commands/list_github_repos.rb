c1555747f842c082bfbdc25fcb68bd10d4ffea30
root <root@sbk-vop-test.(none)>
2012-08-09 16:03:38 +0200

    error handling for list_github_repos

--- github_scm/commands/list_github_repos.rb
+++ github_scm/commands/list_github_repos.rb
@@ -14,7 +14,7 @@ execute do |params|
   
   if params.has_key?('github_user') and params.has_key?('github_password')
     result = JSON.parse(@op.http_get("url" => "https://#{params["github_user"]}:#{params["github_password"]}@api.github.com/user/repos"))
-  elsif params.has_key?('github_token')
+  elsif params.has_key?('github_token') and params["github_token"]
     result = JSON.parse(@op.http_get("url" => "https://api.github.com/user/repos?access_token=#{params["github_token"]}"))
   else
     raise "need either github user/password or access token to authenticate against github"
