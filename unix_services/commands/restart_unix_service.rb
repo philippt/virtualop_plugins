3ebaf973bccc0be04d40ce671de30b049e660c4a
root <root@sbk-vop-test.(none)>
2012-12-04 14:15:16 +0100

    restart_unix_service for SLES

--- unix_services/commands/restart_unix_service.rb
+++ unix_services/commands/restart_unix_service.rb
@@ -5,7 +5,7 @@ param :unix_service, :default_param => true, :allows_multiple_values => true
 
 on_machine do |machine, params|
   case machine.linux_distribution.split("_").first
-  when "centos"
+  when "centos","sles"
     params["name"].each do |name|
       machine.ssh_and_check_result("command" => "/etc/init.d/#{name} restart")
     end
