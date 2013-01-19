5c1995d272acb2cd3460e15e16386b117285fe4b
root <root@dev_vop.(none)>
2013-01-19 19:53:08 +0100

    can't have params in vop_ci right now, also fixed yum_update_log

--- rpm/commands/yum_update_log.rb
+++ rpm/commands/yum_update_log.rb
@@ -6,9 +6,9 @@ display_type :list
 
 on_machine do |machine, params|
   result = []
-  log_name = "/var/log/yum_update.log"
+  log_name = "/var/log/yum_update*.log"
   if machine.file_exists("file_name" => log_name)
-    result += machine.ssh_and_check_result("command" => "tail /var/log/yum_update*.log").split("\n")
+    result += machine.tail(log_name)
   end
   result
 end
