492b8cd402b43f6f451aa3909782041d27a7c3fc
Philipp T <philipp@virtualop.org>
2013-08-30 14:51:15 +0200

    custom apache_config templates in service descriptor

--- service_descriptors/commands/install_service_from_descriptor.rb
+++ service_descriptors/commands/install_service_from_descriptor.rb
@@ -302,6 +302,39 @@ on_machine do |machine, params|
       machine.restart_service("service" => "apache")
       
       machine.configure_reverse_proxy("domain" => domain) if machine.proxy
+    end
+    
+    if service.has_key?("apache_config")
+      unless params.has_key?("extra_params") and params["extra_params"].has_key?("domain")
+        raise "apache_config found for service #{service["name"]}, but no domain parameter is present. don't know where to publish, please give me a domain param"
+      end
+      
+      domain = params["extra_params"]["domain"]
+      if domain.is_a?(Array)
+        domain = domain.first
+      end      
+      machine.install_canned_service("service" => "apache/apache")
+      
+      template_name = service["apache_config"]
+      template_path = descriptor_dir + '/templates/' + template_name.to_s + '.erb'
+      puts "template for apache config : #{template_path}"
+      
+      config_path = "#{machine.apache_generated_conf_dir}/#{domain}.conf"
+      puts "apache config will be written into #{config_path}"
+      
+      generated = @op.with_machine(params["descriptor_machine"]) do |descriptor_machine|
+        descriptor_machine.process_file(
+          "file_name" => template_path,
+          "bindings" => binding() 
+        )
+      end
+      puts "generated : #{generated}"
+      machine.as_user('root') do |root|
+        root.write_file("target_filename" => config_path, "content" => generated)
+      end
+      
+      machine.restart_service("service" => "apache")
+      machine.configure_reverse_proxy("domain" => domain) if machine.proxy
     end    
     
     if service.has_key?("static_html")
--- service_descriptors/helpers/service_descriptor_loader.rb
+++ service_descriptors/helpers/service_descriptor_loader.rb
@@ -167,7 +167,7 @@ class ServiceDescriptorLoader
     targets = [ :redirect_log, :start_command, :stop_command, :on_install ]
     targets += [ :port, :process_regex ]
     targets += [ :cron, :every ]
-    targets += [ :static_html ]
+    targets += [ :static_html, :apache_config ]
     targets += [ :user ]
     
     if targets.include? m
