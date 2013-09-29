ff96fb59c2e5146f149190bb2a1e10b8543e4ebd
Philipp T <philipp@virtualop.org>
2013-09-29 15:09:53 +0200

    let's see if tag_as_stable works again

--- plugin_dev/commands/tag_as_stable.rb
+++ plugin_dev/commands/tag_as_stable.rb
@@ -1,13 +1,14 @@
-description "modifies the configuration of all 'virtualop' working copies to use ssh instead of git as transport layer"
-
 param :machine
 
+param :keypair
+
 on_machine do |machine, params|
-  machine.write_keys
+  @op.prepare_github_ssh_connection(params)
+  
   machine.list_working_copies.each do |wc|
     name = wc["name"]
     details = machine.working_copy_details("working_copy" => name)
-    if details["project"].split("/").first == "virtualop"
+    if details.has_key?("project") && details["project"].split("/").first == "virtualop"
       machine.switch_to_ssh("working_copy" => name)
       machine.tag_working_copy("force" => "true", "working_copy" => name, "tag" => "stable", "comment" => "passed CI #{Time.now.strftime("%Y%m%d")} on #{machine.name}")
     end
