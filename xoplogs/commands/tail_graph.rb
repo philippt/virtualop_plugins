d776ddb131396ce245da17b505e915498dc1d195
Philipp T <philipp@virtualop.org>
2013-10-25 00:02:53 +0200

    tail_graph

--- xoplogs/commands/tail_graph.rb
+++ xoplogs/commands/tail_graph.rb
@@ -5,8 +5,14 @@ param! "path", "path to the logfile (see find_logs)"
 
 on_machine do |machine, params|
   
-  details = @op.service_details("machine" => config_string('xoplogs_machine'), "service" => "xoplogs")
-  xoplogs_url = 'http://' + details["domain"].first.first # TODO we know that one
+  details = @op.service_details("machine" => config_string('xoplogs_machine'), "service" => "xoplogs/xoplogs")
+  domain = details["domain"]
+  # TODO snafu
+  if domain.is_a? Array
+    domain = domain.first
+  end
+  xoplogs_url = 'http://' + domain
+  puts "domain : >>#{domain}<<"
   
   lines = machine.tail("lines" => 1000, "file_name" => params["path"])
   
@@ -14,12 +20,18 @@ on_machine do |machine, params|
   raise "no log file definition found for #{params["path"]} on #{machine.name}" unless log_file
   raise "no parser defined for log file #{params["path"]}" unless log_file.has_key?("parser") and log_file["parser"] != ""
   
-  tempfile = @op.write_tempfile("data" => lines)
-  JSON.parse(@op.http_form_upload(
-    "machine" => "localhost",
-    "target_url" => xoplogs_url + '/import_log/parse_and_aggregate', 
-    "file_name" => tempfile.path,
-    "param_name" => "pic",
-    "extra_content" => "parser=#{log_file["parser"]}"
-  ))
+  #tempfile = @op.write_tempfile("data" => lines)
+  tempfile = "#{machine.home}/tmp/tail_graph.tmp"
+  begin
+    @op.write_file("machine" => "localhost", "target_filename" => tempfile, "content" => lines)
+    JSON.parse(@op.http_form_upload(
+      "machine" => "localhost",
+      "target_url" => xoplogs_url + '/import_log/parse_and_aggregate', 
+      "file_name" => tempfile,
+      "param_name" => "pic",
+      "extra_content" => "parser=#{log_file["parser"]}"
+    ))
+  ensure
+    #@op.rm("machine" => "localhost", "file_name" => tempfile)
+  end
 end  
