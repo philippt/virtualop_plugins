ce24f34519d0948e0223c4d12001ad9014c17f5f
Philipp T <philipp@virtualop.org>
2013-11-02 12:25:30 +0100

    graph_lines performance

--- xoplogs/commands/graph_lines.rb
+++ xoplogs/commands/graph_lines.rb
@@ -13,22 +13,10 @@ on_machine do |machine, params|
   raise "no log file definition found for #{params["path"]} on #{machine.name}" unless log_file
   raise "no parser defined for log file #{params["path"]}" unless log_file.has_key?("parser") and log_file["parser"] != ""
   
-  result = nil
+  uri = URI.parse(xoplogs_url + '/import_log/parse_and_aggregate')
+  response = Net::HTTP.post_form(uri, {"parser" => log_file["parser"], "lines" => lines})
   
-  # TODO should be more original in naming
-  tempfile = "#{@op.home("machine" => "localhost")}/tmp/tail_graph.tmp"
-  begin
-    @op.write_file("machine" => "localhost", "target_filename" => tempfile, "content" => lines)
-    result = JSON.parse(@op.http_form_upload(
-      "machine" => "localhost",
-      "target_url" => xoplogs_url + '/import_log/parse_and_aggregate', 
-      "file_name" => tempfile,
-      "param_name" => "pic",
-      "extra_content" => "parser=#{log_file["parser"]}"
-    ))
-  ensure
-    @op.rm("machine" => "localhost", "file_name" => tempfile)
-  end
+  result = JSON.parse(response.body)
   
   if params["for_flot"] == 'true' && params.has_key?("tz_offset")
     result = access_log_graph_flot(result, params["tz_offset"])
