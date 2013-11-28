3872738ea8847e8980d45c9711625b05000e43a3
Philipp T <philipp@virtualop.org>
2013-11-28 16:09:29 +0100

    dig_both.dont_fail

--- dns/commands/dig_both.rb
+++ dns/commands/dig_both.rb
@@ -3,6 +3,7 @@ description "performs both a forward and reverse lookup for a given IP address o
 param :machine, "the machine to execute 'dig' on", :mandatory => false, :default_value => 'localhost'
 param! "query", "the thing to query for - can be either an IP address or an hostname", :is_default_param => true
 param "name_server", "the nameserver that should be queried"
+param 'dont_fail', 'if set to true, will not raise an exception if the forward lookup returns no results', :default_value => false
 
 mark_as_read_only
 
@@ -18,15 +19,19 @@ on_machine do |machine, params|
   result += machine.dig(options)
       
   $logger.debug "first result : #{result.last}"
-  raise Exception.new("got no forward lookup results for '#{params["query"]}'") unless result.size() > 0
-  
-  second_query = result.last["direction"] == "forward" ? result.last["ip"] : result.last["hostname"]
-  $logger.debug "now querying for #{second_query}"
-  options = {
-    "query" => second_query 
-  }
-  options['name_server'] = params['name_server'] if params.has_key?('name_server')
-  result += machine.dig(options)
+  if result.size() > 0
+    second_query = result.last["direction"] == "forward" ? result.last["ip"] : result.last["hostname"]
+    $logger.debug "now querying for #{second_query}"
+    options = {
+      "query" => second_query 
+    }
+    options['name_server'] = params['name_server'] if params.has_key?('name_server')
+    result += machine.dig(options)
+  else
+    unless params['dont_fail']
+      raise "got no forward lookup results for '#{params["query"]}'"
+    end 
+  end
 
   result
 end
